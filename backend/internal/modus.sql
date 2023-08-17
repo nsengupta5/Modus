54acfc30d426ea72895a2dec7f2d6eed72e56040
Nikhil Sengupta <nikhil.sengupta10@protonmail.com>
2023-08-17 13:12:50 +0800

    Modified gitignore

--- backend/internal/modus.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
-    email VARCHAR(320) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    PRIMARY KEY (email)
-);
